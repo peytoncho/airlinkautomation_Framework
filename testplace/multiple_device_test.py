03a66653da3c1778be3cf003a7e65392202f59ad
peytoncho <peytoncho@gmail.com>
2014-01-14 17:25:53 -0800

    gogo

---
+++ testplace/multiple_device_test.py
@@ -0,0 +1,123 @@
+
+
+
+
+
+class MultipleDeviceTest(object):
+    def __init__(self):
+        return
+    
+    def tc_fwupdate_GX400_MC8705_OSM(self):
+        pass
+    
+    def tc_fwupdate_GX400_MC8705_ATT(self):
+        pass
+    
+    def tc_fwupdate_GX400_MC8705_BEL(self):
+        pass
+    
+    def tc_fwupdate_GX400_MC8705_TLS(self):
+        pass
+    
+    def tc_fwupdate_GX400_MC8705_OSM(self):
+        pass
+       
+    def tc_fwupdate_GX400_MC5728_VZW(self):
+        pass
+    
+    def tc_fwupdate_GX440_MC7750_VZW(self):
+        pass
+    
+    def tc_fwupdate_GX440_MC7700_ATT(self):
+        pass
+    
+    def tc_fwupdate_GX400_MC5728_SPT(self):
+        pass
+
+    def tc_fwupdate_GX440_MC7700_OSM(self):
+        pass
+
+    def tc_fwupdate_ES440_MC7750_VZW(self):
+        pass
+    
+    def tc_fwupdate_ES440_MC7700_ATT(self):
+        pass
+
+    def tc_fwupdate_ES440_MC7710_EMEA(self):
+        pass
+
+    def tc_fwupdate_ES440_MC7700_OSM(self):
+        pass   
+
+    def tc_fwupdate_LS300_SL5011_VZW(self):
+        pass 
+
+    def tc_fwupdate_LS300_SL5011_SPT(self):
+        pass 
+
+    def tc_fwupdate_LS300_SL8090_ATT(self):
+        pass 
+
+    def tc_fwupdate_LS300_SL8090_BEL(self):
+        pass 
+
+    def tc_fwupdate_LS300_SL8092_OSM(self):
+        pass 
+
+
+
+
+#===============================================================================
+# Launcher section
+#===============================================================================
+testing_combo = mdt_config_map["TESTING_COMBO"]
+
+testcase_map = {
+    1:[MultipleDeviceTest,"tc_fwupdate_GX400_MC8705_OSM",0],
+    2:[MultipleDeviceTest,"tc_fwupdate_GX400_MC8705_ATT",0],
+    3:[MultipleDeviceTest,"tc_fwupdate_GX400_MC8705_BEL",0],
+    4:[MultipleDeviceTest,"tc_fwupdate_GX400_MC8705_TLS",0],
+    5:[MultipleDeviceTest,"tc_fwupdate_GX400_MC8705_OSM",0],
+    6:[MultipleDeviceTest,"tc_fwupdate_GX400_MC5728_VZW",0],
+    7:[MultipleDeviceTest,"tc_fwupdate_GX440_MC7750_VZW",0],
+    8:[MultipleDeviceTest,"tc_fwupdate_GX440_MC7700_ATT",0],
+    9:[MultipleDeviceTest,"tc_fwupdate_GX400_MC5728_SPT",0],
+    10:[MultipleDeviceTest,"tc_fwupdate_GX440_MC7700_OSM",0],
+    11:[MultipleDeviceTest,"tc_fwupdate_ES440_MC7750_VZW",0],
+    12:[MultipleDeviceTest,"tc_fwupdate_ES440_MC7700_ATT",0],
+    13:[MultipleDeviceTest,"tc_fwupdate_ES440_MC7710_EMEA",0],
+    14:[MultipleDeviceTest,"tc_fwupdate_ES440_MC7700_OSM",0],
+    15:[MultipleDeviceTest,"tc_fwupdate_GX440_MC7700_OSM",0],
+    16:[MultipleDeviceTest,"tc_fwupdate_LS300_SL5011_VZW",0],
+    17:[MultipleDeviceTest,"tc_fwupdate_LS300_SL5011_SPT",0],
+    18:[MultipleDeviceTest,"tc_fwupdate_LS300_SL8090_ATT",0],
+    19:[MultipleDeviceTest,"tc_fwupdate_LS300_SL8090_BEL",0],
+    20:[MultipleDeviceTest,"tc_fwupdate_LS300_SL8092_OSM",0],
+}
+
+if testing_combo == "DEVICE_COMBO1":
+    args = "1-5"    
+elif testing_combo == "DEVICE_COMBO2":
+    args = "6-10"
+elif testing_combo == "DEVICE_COMBO3":
+    args = "11-15"
+else:
+    args = "16-20"
+    
+IP_map = {
+      1: "192.168.13.1",
+      2: "192.168.13.2",         
+      3: "192.168.13.3",
+      4: "192.168.13.4",
+      5: "192.168.13.5",      
+    }
+
+
+
+
+
+
+
+    
+    
+     
