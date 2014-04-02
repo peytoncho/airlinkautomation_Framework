856771a3bc5a533a8c523bfa3e6a7146cba5bf15
peytoncho <peytoncho@gmail.com>
2014-04-01 17:42:21 -0700

    jjj

--- testsuite/Feature/Fwupdate/testsuite_fwupdate_Launcher.py
+++ testsuite/Feature/Fwupdate/testsuite_fwupdate_Launcher.py
@@ -101,67 +101,96 @@ def dump_tc_list(combo_list):
 #  Firmware Update test automation main
 ####################################################
 
-if __name__ == "__main__":
+class Runner(object):
+    def __init__(self,device="",test_type):
+        self.device = device
+        self.test_type = test_type
+        if tbd_config_map["LOG_LEVEL"]=="DEBUG":
+            self.LEVEL = logging.DEBUG
+        else: 
+            self.LEVEL = logging.INFO
+        
+        self.FORMAT ='%(asctime)-15s => %(levelname)-8s => %(message)s'
     
-    if fwupdate_config_map["MDT"] == "YES":
-        mdt_ins = mdt_airlink.MdtAirlink()
-        
-        #1, change all connected devices IP
-        mdt_ins.change_global_ip()
-        print "IP changed Waiting..."
-        time.sleep(90)
-         
-        #2, check devices connection
-        check_connection_flag = mdt_ins.ping_devices()
-        if not check_connection_flag:
-            sys.exit(2)
-        
-        #3, form the devices list 
-        combo_list = mdt_ins.form_device_fullname()
-        
-        #4, pick the test case
-        pick_list = pickTestcase(combo_list)
-        
-        #5,write the list to yml
-        dump_tc_list(combo_list)
-        
-        mySuite = basic_airlink.setup_suite_mdt(tc_ts_map, pick_list)
-    else:
-        mySuite = basic_airlink.setup_suite(tbd_config_map,fwupdate_config_map, tc_ts_map)
-                 
-    log_filename=basic_airlink.get_log_filename(tbd_config_map, test_area,"")
-    FORMAT ='%(asctime)-15s => %(levelname)-8s => %(message)s'
-    if tbd_config_map["LOG_LEVEL"]=="DEBUG":
-        LEVEL = logging.DEBUG
-    else: 
-        LEVEL = logging.INFO 
-    logging.basicConfig(level = LEVEL,filename = log_filename, format=FORMAT,  filemode='w')    
-    time_stamp = time.strftime("%b-%d-%Y_%H-%M")
-    report_filename=basic_airlink.get_report_filename(tbd_config_map, test_area,"")
-    report_file_name = report_filename.split('/')[-1]
-    fpp = file(report_filename, 'wb')   
-    description_text= r""" ***"""+ "log file name " +log_filename
-           
-    runner = htmlreport.HTMLTestRunner(
+    def create_log_name(self):
+        log_filename=basic_airlink.get_log_filename(tbd_config_map, test_area,"")          
+        return log_filename
+    
+    def create_report_name(self):
+        time_stamp = time.strftime("%b-%d-%Y_%H-%M")
+        report_filename=basic_airlink.get_report_filename(tbd_config_map, test_area,"")
+#        report_file_name = report_filename.split('/')[-1]
+        return report_filename
+    
+    def run(self):
+        log_filename = self.create_log_name()
+        report_filename = self.create_report_name()
+        fpp = file(report_filename, 'wb')
+        logging.basicConfig(level = self.LEVEL,filename = log_filename, format=self.FORMAT,  filemode='w')
+        description_text= r""" ***"""+ "log file name " +log_filename
+        runner = htmlreport.HTMLTestRunner(
                 stream = fpp,
                 title = 'Firmware Update Test Report', 
                 description = description_text
-                )       
+                )
+        if self.test_type == "mdt":
+            mySuite = basic_airlink.setup_suite_mdt(tc_ts_map, pick_list)
+        else:
+            mySuite = basic_airlink.setup_suite(tbd_config_map,fwupdate_config_map, tc_ts_map)
+        test_cases = mySuite.countTestCases()   
+        basic_airlink.slog("\x1b[0mTotal test cases: %d" % test_cases)
+        test_result=runner.run(mySuite)
+        fpp.close()
+        basic_airlink.slog("\x1b[0mTotal %d test cases PASS." % test_result.success_count )
+        basic_airlink.slog("Total %d test cases FAILED." % test_result.failure_count )
+        basic_airlink.slog("Total %d test cases has ERROR." % test_result.error_count )   
+
+
+
+class Launcher(object):
+    def __init__(self,test_type):
+        self.test_type = test_type
+        if self.test_type == "mdt":
+            self.mdt_ins = mdt_airlink.MdtAirlink.__init__()
     
-    test_cases = mySuite.countTestCases()
-    basic_airlink.slog("\x1b[0mTotal test cases: %d" % test_cases)
-      
-    test_result=runner.run(mySuite)
-    basic_airlink.slog("\x1b[0mTotal %d test cases PASS." % test_result.success_count )
-    basic_airlink.slog("Total %d test cases FAILED." % test_result.failure_count )
-    basic_airlink.slog("Total %d test cases has ERROR." % test_result.error_count )
-    fpp.close()
-#    shutil.copyfile('C:'+report_filename, 'C:/jenkins/workspace/Firmware_update_test/reports/'+report_file_name)
-#    sys.stdout.write("\nFor details of the results please check \n http://carmd-ev-aptest:8080/job/Firmware_update_test/ws/reports/%s\n\n For details of the log please check \n http://carmd-ev-aptest:8080/job/Firmware_update_test/ws/logs/%s\n\n"  % ( report_file_name,log_filename))    
+    def run(self):
+        if self.test_type == "mdt":
+        
+            #1, change all connected devices IP
+            self.mdt_ins.change_global_ip()
+            print "IP changed Waiting..."
+            time.sleep(90)
+         
+            #2, check devices connection
+            check_connection_flag = self.mdt_ins.ping_devices()
+            if not check_connection_flag:
+                sys.exit(2)
+        
+            #3, form the devices list 
+            combo_list = mdt_ins.form_device_fullname()
+        
+            #4, pick the test case
+            pick_list = pickTestcase(combo_list)
+        
+            #5,write the list to yml
+            dump_tc_list(combo_list)
+            
+            device_list = []
+                        
+            for device in device_list:
+                
+                dev_runner = Runner(device,self.test_type).run()
+        else:
+             pass      
+         
+       
+
+if __name__ == "__main__":
+    if fwupdate_config_map["MDT"] == "YES":    
+        test_type = "mdt"
+    else:
+        test_type = "single"
     
+    Launcher(test_type).run()
     #mdt_ins.restore_device_ip()
     
-    if (test_result.error_count + test_result.failure_count): 
-        sys.exit(1)
-    else:
-        sys.exit(0)
